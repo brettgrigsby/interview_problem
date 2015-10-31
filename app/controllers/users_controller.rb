47f89849c03ee0d46ab869656180671a61594492
brettgrigsby <gbrett@gmail.com>
2015-10-31 15:09:33 -0600

    user creation works and responds with json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,19 @@ class UsersController < ApplicationController
     user = User.where(id: params[:id]).select(:id, :first_name, :last_name, :email).first
     render json: user
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: User.where(id: user.id).select(:id, :first_name, :last_name, :email).first
+    else
+      render text: "Invalid Params"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :social_security_number)
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,4 +10,23 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "create user by sneding json" do
+    post :create, { 'user' => { 'first_name' => 'Create',
+                                'last_name' => 'User',
+                                'email' => 'created@test.com',
+                                'social_security_number' => '123456789' }}, :format => :json
+
+    assert_response :success
+
+    user_output = JSON.parse(@response.body)
+    assert_equal "Create", user_output["first_name"]
+    assert_equal "User", user_output["last_name"]
+    assert_equal "created@test.com", user_output["email"]
+    refute user_output["social_security_number"]
+
+    created_user = User.find_by(first_name: "Create")
+    assert_equal "created@test.com", created_user.email
+    
+  end
+
 end
