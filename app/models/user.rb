a1715ada270e4c8622ee838ca16b3a81d4ea97dc
brettgrigsby <gbrett@gmail.com>
2015-10-31 12:14:28 -0600

    created and tested user model and validation

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: /@/ }
+  validates :social_security_number, presence: true, length: { is: 9 }
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new(first_name: "Brett", last_name: "Grigsby", email: "test@email.com", social_security_number: "123456789")
+  end
+
+  test "user is valid" do
+    assert @user.valid?
+  end
+
+  test "user invalid without first name" do
+    @user.first_name = nil
+    refute @user.valid?
+  end
+
+  test "user invalid without last name" do
+    @user.last_name = nil
+    refute @user.valid?
+  end
+
+  test "user invalid without email" do
+    @user.email = nil
+    refute @user.valid?
+  end
+
+  test "user invalid with improper email" do
+    @user.email = "thisaintnoemail"
+    refute @user.valid?
+  end
+
+  test "user invalid without social" do
+    @user.social_security_number = nil
+    refute @user.valid?
+  end
+
+  test "user invalid with improper social" do
+    @user.social_security_number = "12345"
+    refute @user.valid?
+  end
+end
